286fc962553d7dba428c1fee835281d0a7a056ad
Jay <prasanth.m3669@gmail.com>
2024-01-31 22:19:27 +0530

    H22

--- routers/designation.router.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createDesignation } from "../controllers/designation.controller.js";
-
-const router = express.Router();
-
-router.post("/createDesignation", createDesignation);
-
-export default router;
